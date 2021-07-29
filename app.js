f4c297ae5878183517dbfec8764f1b4f6d9ba2d0
dp00t <86521685+dp0ttxy@users.noreply.github.com>
2021-07-29 13:19:31 -0400

    core concept
    idea works on here

---
+++ app.js
@@ -0,0 +1,98 @@
+var axios = require("axios").default;
+var inquirer = require("inquirer");
+
+function bigquestion() {
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Hey, what video game that you dont own are you curious about? (if it contains spaces, use dashes instead!)",
+      name: "gamename"
+    },
+    {
+      type: "confirm",
+      message: "Would you like to find out the best place to buy that game?",
+      name: "confirm",
+      default: true
+    }
+  ])
+  .then(function(inquirerResponse) {
+    if (inquirerResponse.confirm) {
+      var options = {
+        method: 'GET',
+        url: 'https://game-prices.p.rapidapi.com/game/'+inquirerResponse.gamename,
+        params: {region: 'us', type: 'game'},
+        headers: {
+          'x-rapidapi-key': '40db8a69bdmshbf4d7c2d3d7e633p11e2bajsn07e4904114cb',
+          'x-rapidapi-host': 'game-prices.p.rapidapi.com'
+        }
+      };
+      console.log("Alright, ill show you!\n");
+      axios.request(options).then(function (response) {
+        console.log(response.data);
+        question();
+      }).catch(function (error) {
+        console.error("Sorry, we couldn't find that game. :(");
+        question();
+      });
+    }
+    else {
+      console.log("Alright, no worries");
+    }
+  });
+}
+function diggquestion() {
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Whats it called? (if it contains spaces, use dashes instead!)",
+      name: "gamename"
+    }
+  ])
+  .then(function(inquirerResponse) {
+      var options = {
+        method: 'GET',
+        url: 'https://game-prices.p.rapidapi.com/game/'+inquirerResponse.gamename,
+        params: {region: 'us', type: 'game'},
+        headers: {
+          'x-rapidapi-key': '40db8a69bdmshbf4d7c2d3d7e633p11e2bajsn07e4904114cb',
+          'x-rapidapi-host': 'game-prices.p.rapidapi.com'
+        }
+      };
+      console.log("Alright, ill show you!\n");
+      axios.request(options).then(function (response) {
+        console.log(response.data);
+        question();
+      }).catch(function (error) {
+        console.error("Sorry, we couldnt find that game. :(");
+        question();
+      });
+    
+  });
+}
+function question() {
+  inquirer
+  .prompt([
+    {
+      type: "confirm",
+      message: "do you want to find another game?",
+      name: "confirm2"
+    }
+  ])
+  .then(function(inquirerResponse) { 
+  if (inquirerResponse.confirm2) {
+      diggquestion();
+  } else {
+    console.log("alright, hope that helped");
+  }
+})
+}
+
+bigquestion();
+
+
+
+
+
+
