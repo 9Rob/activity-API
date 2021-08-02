248fe755430ecf066558ec910d8248b2fdc6a767
dp00t <86521685+dp0ttxy@users.noreply.github.com>
2021-08-02 08:55:21 -0400

    final version
    added 2 new functions, findgameid and alert.

--- app.js
+++ app.js
@@ -1,6 +1,97 @@
 var axios = require("axios").default;
 var inquirer = require("inquirer");
 
+function alert() {
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "what price are you wishing for? (must be a decimal, no dollar or similar signs)",
+      name: "price"
+    },
+    {
+      type: "input",
+      message: "what's the email that you want to be notified on?",
+      name: "email"
+    },
+    {
+      type: "input",
+      message: "whats the id of the game you wanted?",
+      name: "gameid"
+    }
+  ])
+  .then(function(inquirerResponse) {
+      var options = {
+        method: 'GET',
+        url: 'https://cheapshark-game-deals.p.rapidapi.com/alerts',
+        params: {action: 'set', email: inquirerResponse.email, gameID: inquirerResponse.gameid, price: inquirerResponse.price},
+        headers: {
+          'x-rapidapi-key': '40db8a69bdmshbf4d7c2d3d7e633p11e2bajsn07e4904114cb',
+          'x-rapidapi-host': 'cheapshark-game-deals.p.rapidapi.com'
+        }
+      };
+      axios.request(options).then(function (response) {
+        console.log(response.data);
+        if (response.data === true) {
+          console.log("Alert set");
+          inquirer
+        .prompt([
+    {
+      type: "confirm",
+      message: "do you want to make another alert?",
+      name: "confirm4"
+    }
+  ])
+  .then(function(inquirerResponse) { 
+    if (inquirerResponse.confirm4) {
+    findgameid();
+    } else {
+      console.log("alright, hope this app helped you in any way, shape, or form");
+    }
+  })
+        } else {
+          console.log("Please make sure you inputted your details correctly.");
+          alert();
+        }
+      }).catch(function (error) {
+        console.error("something went wrong, please make sure you inputted everything correctly");
+        alert();
+      });
+
+  });
+}
+
+function findgameid() {
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "whats the name of the game? You will need to get the ID of this game in order to make the alert.",
+      name: "game"
+    }
+  ])
+  .then(function(inquirerResponse) {
+      var options = {
+        method: 'GET',
+        url: 'https://cheapshark-game-deals.p.rapidapi.com/games',
+        params: {title: inquirerResponse.game, exact: '0', limit: '60'},
+        headers: {
+          'x-rapidapi-key': '40db8a69bdmshbf4d7c2d3d7e633p11e2bajsn07e4904114cb',
+          'x-rapidapi-host': 'cheapshark-game-deals.p.rapidapi.com'
+        }
+      };
+      
+      axios.request(options).then(function (response) {
+        console.log(response.data);
+        console.log("remember this number, youll need it for your alert!");
+        alert();
+      }).catch(function (error) {
+        console.error("sorry, something went wrong, make sure you typed everything correctly.");
+        findgameid();
+      });
+  });
+}
+
 function bigquestion() {
   inquirer
   .prompt([
@@ -84,8 +175,22 @@ function question() {
   if (inquirerResponse.confirm2) {
       diggquestion();
   } else {
-    console.log("alright, hope that helped");
+    inquirer
+  .prompt([
+    {
+      type: "confirm",
+      message: "do you want to make an alert for a game you found to see if it lowers in price even more?",
+      name: "confirm3"
+    }
+  ])
+  .then(function(inquirerResponse) { 
+  if (inquirerResponse.confirm3) {
+  findgameid();
+  } else {
+    console.log("alright, hope this app helped you in any way, shape, or form");
   }
+})
+}
 })
 }
 
